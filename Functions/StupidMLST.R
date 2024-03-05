b0add064d464dadf57ed14fbcc24b9ff17cd51c5
MAphd <98038096+MAphd@users.noreply.github.com>
2024-03-05 16:37:18 +0100

    Update StupidMLST.R
    See issue #1

--- Functions/StupidMLST.R
+++ Functions/StupidMLST.R
@@ -143,14 +143,79 @@ for(i in 1:length(Seqnms)){
       
       # a[seq(2,length(a),2)]
       
-      seqST <- as.numeric(a[seq(2,length(a),2)])
+      seqST <- as.numeric(a[c(FALSE,TRUE)])
     } else {
       seqST <- as.numeric(c(1,0,0,0,0,0,0) ) # if 7 unique alleles cant be found
     }
     
     
   } else {
-    seqST <- as.numeric(c(0,1,0,0,0,0,0) ) # if we get more or less than 7 perfect hits 
+    # if we get more or less than 7 perfect hits 
+    seqST <- as.numeric(c(0,1,0,0,0,0,0) )
+    # Is there multiple hits for some alleles and more than 7 hits? last ditch effort to assign a ST
+    if(  dim((Data[Data$Mismatch==0&Data$Gaps==0&Data$Coverage==100,]))[1] > as.integer(7) & 
+         any(duplicated(unlist(strsplit(Data$Subjacc[Data$Mismatch==0&Data$Gaps==0&Data$Coverage==100],"_"))[c(TRUE,FALSE)]))
+    ){
+      
+      #Index of duplicated allele
+      b <- duplicated(unlist(strsplit(Data$Subjacc[Data$Mismatch==0&Data$Gaps==0&Data$Coverage==100],"_"))[c(TRUE,FALSE)])
+      #if more than one duplicated allele, deem untypable (ie ST 9999)
+      if( identical(sum(b),as.integer(1)) ){
+        
+        
+        
+        #check possible MLSTs 
+        a <- strsplit(Data$Subjacc[Data$Mismatch==0&Data$Gaps==0&Data$Coverage==100],"_")
+        #duplicated allele name
+        a1 <- a[[which(b)]][[1]]
+        
+        #indexes of duplicated allele
+        b1 <- which(unlist(strsplit(Data$Subjacc[Data$Mismatch==0&Data$Gaps==0&Data$Coverage==100],"_"))[c(TRUE,FALSE)]%in%a1)
+        
+        a2 <- unlist(strsplit( sort(Data[Data$Mismatch==0&Data$Gaps==0&Data$Coverage==100,]$Subjacc[-(b1[1])]) , "_" ))
+        if(    identical(length(unique(a2[seq(1,length(a2),2)])),as.integer(7))  ){
+          seqST1 <- as.numeric(a2[c(FALSE,TRUE)])
+          
+        }
+        
+        a3 <- unlist(strsplit( sort(Data[Data$Mismatch==0&Data$Gaps==0&Data$Coverage==100,]$Subjacc[-(b1[2])]) , "_" ))
+        if(    identical(length(unique(a3[seq(1,length(a3),2)])),as.integer(7))  ){
+          
+          seqST2 <- as.numeric(a3[c(FALSE,TRUE)])
+          
+        }
+        
+        #Check if MLST exists for both allele combinations
+        check1 <- DST$ST[DST$acsA==seqST1[1]&DST$aroE==seqST1[2]&DST$guaA==seqST1[3]&DST$mutL==seqST1[4]&DST$nuoD==seqST1[5]&DST$ppsA==seqST1[6]&DST$trpE==seqST1[7]]
+        check2 <- DST$ST[DST$acsA==seqST2[1]&DST$aroE==seqST2[2]&DST$guaA==seqST2[3]&DST$mutL==seqST2[4]&DST$nuoD==seqST2[5]&DST$ppsA==seqST2[6]&DST$trpE==seqST2[7]]
+        
+        #Check if any MLSTs were assigned
+        if( any((!c(identical(check1,numeric(0)),identical(check2,numeric(0))))) ){
+          
+          #If more than 1 MLSTs can be assigned, there are duplicate alleles and the genome is untypable so we don't proceed
+          if( !identical(sum((!c(identical(check1,numeric(0)),identical(check2,numeric(0))))),as.integer(2)) ){
+            
+            if( identical(which(!c(identical(check1,numeric(0)),identical(check2,numeric(0)))),as.integer(1)) ){
+              
+              seqST <- as.numeric(a2[c(FALSE,TRUE)])
+            } else if( identical(which(!c(identical(check1,numeric(0)),identical(check2,numeric(0)))),as.integer(2)) ){
+              
+              seqST <- as.numeric(a3[c(FALSE,TRUE)])
+            }
+            
+            
+          }
+          
+          
+        }
+        
+        
+        
+        
+        rm(a1);rm(a2);rm(a3);rm(b);rm(b1);rm(seqST1);rm(seqST2);rm(check1);rm(check2)
+      }
+    }
+    
   }
   
   
